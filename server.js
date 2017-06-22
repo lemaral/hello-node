b090551bfb27df604c1ad31ab3a8162561620c55
lemaral <lemaral@users.noreply.github.com>
2017-06-23 00:06:36 +0200

    v0.4.0

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello, Node! v3');
+  response.end('Hello, Node! v0.4.0');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
