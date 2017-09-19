8b3e9a8690a5b3e33e08963c84066d21839a177a
Alexandre Lemaresquier <alex+bitbucket@lemaresquier.org>
2017-09-19 14:49:55 +0200

    Add port to stdout

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ http.createServer(on8080).listen(8080);
 http.createServer(on8081).listen(8081);
 
 function on8080(request, response) {
-  console.log('Received request for URL: ' + request.url);
+  console.log('8080: Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('8080: Hello, Node! v0.4.2');
-};
+  response.end('8080: Hello, Node! v0.4.3');
+}
 
 function on8081(request, response) {
-  console.log('Received request for URL: ' + request.url);
+  console.log('8081: Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('8081: Hello, Node! v0.4.2');
-};
+  response.end('8081: Hello, Node! v0.4.3');
+}
